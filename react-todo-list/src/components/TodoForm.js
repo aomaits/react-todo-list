1e2aff3ee06daa23cab7935f06a6f246d5fa33e2
Abe Omaits <aomaits@gmail.com>
2024-02-03 16:00:28 -0600

    removed the random math to try and set up an incrementally counting id

--- react-todo-list/src/components/TodoForm.js
+++ react-todo-list/src/components/TodoForm.js
@@ -3,21 +3,24 @@ import React, { useState } from 'react'
 function TodoForm(props) {
   // set initial input to empty
   const [input, setInput] = useState('');
-
+  const [idNumber, setIdNumber] = useState(1);
+  
   const handleChange = e => {
     setInput(e.target.value);
   }
-
+  
   //prevent page refresh on the form
   const handleSubmit = e => {
     e.preventDefault();
-
+    
     props.onSubmit({
-      // create random id
-      id: Math.floor(Math.random() * 10000),
+      id: idNumber,
       text: input
     });
-
+    
+    
+    //idNumber incrementally increases
+    setIdNumber(prevState => ( prevState + 1))
     // after user submits, empty input form
     setInput('');
   };
--- react-todo-list/src/components/TodoList.js
+++ react-todo-list/src/components/TodoList.js
@@ -4,10 +4,22 @@ import TodoForm from './TodoForm'
 function TodoList() {
   const [todos, setTodos] = useState([]);
 
+  const addTodo = todo => {
+    // if no text OR if whitespace character
+    if(!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+    console.log(...todos)
+  }
+
   return (
     <div>
       <h1>What's the Plan for Today?</h1>
-      <TodoForm />
+      <TodoForm onSubmit={addTodo}/>
     </div>
   )
 }
