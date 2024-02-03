141aab00c97b7c0c98fbc1831f1633f75aaa481b
Abe Omaits <aomaits@gmail.com>
2024-02-03 16:43:59 -0600

    added update and delete functionality to todo list but seeing odd duplications

--- react-todo-list/src/components/Todo.js
+++ react-todo-list/src/components/Todo.js
@@ -4,11 +4,23 @@ import { RiCloseCircleLine } from 'react-icons/ri';
 import { TiEdit } from "react-icons/ti";
 
 
-function Todo({todos, completeTodo}) {
+function Todo({todos, completeTodo, removeTodo, updateTodo}) {
   const [edit, setEdit] = useState({
     id: null,
     value: ''
   });
+
+  const submitUpdate = value => {
+    updateTodo(edit.id, value)
+    setEdit({
+      id: null,
+      value: ''
+    });
+  };
+
+  if (edit.id) {
+    return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+  };
 
 
   return todos.map((todo, index) => (
@@ -17,8 +29,13 @@ function Todo({todos, completeTodo}) {
       {todo.text}
     </div>
     <div className='icons'>
-      <RiCloseCircleLine />
-      <TiEdit />
+      <RiCloseCircleLine 
+      onClick={() => removeTodo(todo.id)}
+      className='delete-icon'
+      />
+      <TiEdit 
+      onClick={() => setEdit({ id: todo.id, value: todo.text})}
+      className='edit-icon'/>
     </div>
   </div>
   )) 
--- react-todo-list/src/components/TodoList.js
+++ react-todo-list/src/components/TodoList.js
@@ -9,13 +9,29 @@ function TodoList() {
     // if no text OR if whitespace character
     if(!todo.text || /^\s*$/.test(todo.text)) {
       return;
-    }
+    };
 
     const newTodos = [todo, ...todos];
 
     setTodos(newTodos);
-    // console.log(todo, ...todos)
   };
+
+  const updateTodo = (todoId, newValue) => {
+    // if no text OR if whitespace character
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    };
+
+    setTodos(prev => prev.map(item=> (item.id === todoId ? newValue : item)));
+
+  }
+
+  const removeTodo = id => {
+    const removeArr = [...todos].filter(todo => todo.id !== id)
+
+    setTodos(removeArr)
+  };
+
 
   const completeTodo = id => {
     let updatedTodos = todos.map(todo => {
@@ -25,7 +41,7 @@ function TodoList() {
       return todo;
     })
     setTodos(updatedTodos);
-  }
+  };
 
   return (
     <div>
@@ -34,6 +50,8 @@ function TodoList() {
       <Todo 
         todos={todos}
         completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
       />
     </div>
   )
