f5947f3efeda53887360e72551e95a65d6eced22
Abe Omaits <aomaits@gmail.com>
2024-02-03 15:37:33 -0600

    started TodoList component, takes in the TodoForm component

--- react-todo-list/src/components/TodoForm.js
+++ react-todo-list/src/components/TodoForm.js
@@ -1,16 +1,36 @@
 import React, { useState } from 'react'
 
-function TodoForm() {
+function TodoForm(props) {
+  // set initial input to empty
   const [input, setInput] = useState('');
 
+  const handleChange = e => {
+    setInput(e.target.value);
+  }
+
+  //prevent page refresh on the form
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    props.onSubmit({
+      // create random id
+      id: Math.floor(Math.random() * 10000),
+      text: input
+    });
+
+    // after user submits, empty input form
+    setInput('');
+  };
+
   return (
-    <form className='todo-form'>
+    <form className='todo-form' onSubmit={handleSubmit}>
       <input
         type='text'
         placeholder='Add a todo'
         value={input} 
         name='text'
         className='todo-input'
+        onChange={handleChange}
       />
       <button className='todo-button'>Add todo</button>
     </form>
